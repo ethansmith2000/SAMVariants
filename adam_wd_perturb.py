4b037689eac238f6546521fa2c68819e0e641bdf
ethansmith2000 <ethansmith2000@gmail.com>
2025-02-24 18:24:51 +0000

    fix more typos/issues

--- adam_wd_perturb.py
+++ adam_wd_perturb.py
@@ -2,34 +2,6 @@ from typing import Generator
 
 import torch
 
-# https://github.com/KellerJordan/Muon/blob/master/muon.py
-
-
-# @torch.compile
-def zeropower_via_newtonschulz5(G, steps=10, eps=1e-7):
-    """
-    Newton-Schulz iteration to compute the zeroth power / orthogonalization of G. We opt to use a
-    quintic iteration whose coefficients are selected to maximize the slope at zero. For the purpose
-    of minimizing steps, it turns out to be empirically effective to keep increasing the slope at
-    zero even beyond the point where the iteration no longer converges all the way to one everywhere
-    on the interval. This iteration therefore does not produce UV^T but rather something like US'V^T
-    where S' is diagonal with S_{ii}' ~ Uniform(0.5, 1.5), which turns out not to hurt model
-    performance at all relative to UV^T, where USV^T = G is the SVD.
-    """
-    assert len(G.shape) == 2
-    a, b, c = (3.4445, -4.7750, 2.0315)
-    X = G.bfloat16()
-    X /= X.norm() + eps  # ensure top singular value <= 1
-    if G.size(0) > G.size(1):
-        X = X.T
-    for _ in range(steps):
-        A = X @ X.T
-        B = b * A + c * A @ A
-        X = a * X + B @ X
-    if G.size(0) > G.size(1):
-        X = X.T
-    return X
-
 
 class Muon(torch.optim.Optimizer):
     """
@@ -109,6 +81,7 @@ class Muon(torch.optim.Optimizer):
 
                 # remove last ADAM perturbation, 
                 denom = state["exp_avg_sq"].sqrt().add_(group["eps"])
+                # notice subtle lr is postivie instead of negative 
                 param.addcdiv_(state["exp_avg"], denom, value=group["perturb_lr"])
 
                 ############################################################
@@ -152,7 +125,6 @@ class Muon(torch.optim.Optimizer):
 
                 # Do adam perturbation
                 denom = state["exp_avg_sq"].sqrt().add_(group["eps"])
-                # notice subtle lr is postivie instead of negative 
-                param.data.addcdiv_(state["exp_avg"], denom, value=group["perturb_lr"])
+                param.data.addcdiv_(state["exp_avg"], denom, value=-group["perturb_lr"])
 
 
